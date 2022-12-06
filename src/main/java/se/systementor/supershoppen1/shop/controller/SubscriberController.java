70a9fa8289a656ef590bd1a222d620de2fd8123f
Oggilit0 <oskar.herdenberg@gmail.com>
2022-12-06 10:02:42 +0100

    Added pathvariable to getSubscriber

--- src/main/java/se/systementor/supershoppen1/shop/controller/SubscriberController.java
+++ src/main/java/se/systementor/supershoppen1/shop/controller/SubscriberController.java
@@ -3,6 +3,7 @@ package se.systementor.supershoppen1.shop.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import se.systementor.supershoppen1.shop.model.Subscriber;
@@ -25,8 +26,8 @@ public class SubscriberController {
         return subscriberService.getAll();
     }
 
-    @GetMapping(path="/get")
-    public Subscriber getSubscriber(Integer id){
+    @GetMapping(path="/get/{id}")
+    public Subscriber getSubscriber(@PathVariable Integer id){
         return subscriberService.getSubscriber(id);
     }
 
