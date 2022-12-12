72bbf8c81544803ddafad6e895c6db3d158ab5bb
Mathilda <frknilsson@hotmail.com>
2022-12-12 16:36:33 +0100

    added popup to display message from saveSubscriber

--- src/main/java/se/systementor/supershoppen1/shop/controller/SubscriberController.java
+++ src/main/java/se/systementor/supershoppen1/shop/controller/SubscriberController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import se.systementor.supershoppen1.shop.model.Subscriber;
 import se.systementor.supershoppen1.shop.services.SubscriberService;
 
@@ -26,18 +27,19 @@ public class SubscriberController {
 
 
     @RequestMapping(value="/save", method = RequestMethod.POST)
-    public String saveEmail(@ModelAttribute("Newsletter")Subscriber subscriber, Model model,
+    public String saveEmail(@ModelAttribute("Newsletter")Subscriber subscriber, Model model, RedirectAttributes redirAttrs,
                             @RequestParam(name = "addingsub") String email){
 
         String emailRegex = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
-        boolean exists = checkIfEmailExists(email);
-        if(exists){
-            model.addAttribute("checkEmail", exists);
+        boolean emailExists = checkIfEmailExists(email);
+        if(emailExists){
+            model.addAttribute("checkEmail", "1");
         }else if(email.matches(emailRegex)){
             subscriber.setEmail(email);
             subscriberService.save(subscriber);
+            model.addAttribute("checkEmail", "3");
         }else{
-            model.addAttribute("checkEmail", exists);
+            model.addAttribute("checkEmail", "2");
         }
         return "home";
     }
