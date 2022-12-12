8ded8839f4da3f7f3eb0bb10bdb18243c39a153b
Oskar Herdenberg <90776459+Oggilit0@users.noreply.github.com>
2022-12-12 10:41:46 +0100

    Save newsletter draft (#20)
    * Added Boolean Sent to newsletter Db
    
    * Added template for edit newsletter
    
    * Added date format to newsletter table, added edit button to editable post only, added delete button that doesnt work right now
    
    * Added a method to get the last newsletter submitted
    
    * rewrote sendnewsletter to include edits, added editnewsletter method, added getlastnewslettermetod and added help metod to sendnewsletter called savenewsletter
    
    * changed send-newspaper to have an save draft button
    
    * removed some rogue text
    
    * fixed bug with table not refreshing
    
    * logo now redirects to home
    
    * t s:wq

--- src/main/java/se/systementor/supershoppen1/shop/configuration/WebSecurityConfig.java
+++ src/main/java/se/systementor/supershoppen1/shop/configuration/WebSecurityConfig.java
@@ -45,7 +45,7 @@ public class WebSecurityConfig  {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-					.antMatchers("/","/*", "/css/**", "/images/**", "/lib/**", "/scripts/**", "/static/**", "/subscriber/**").permitAll()
+					.antMatchers("/","/*", "/css/**", "/images/**", "/lib/**", "/scripts/**", "/static/**", "/subscriber/save").permitAll()
 					.antMatchers("/admin/**").hasAnyRole("ADMIN")
 					.antMatchers("/user/**").hasAnyRole("USER")
 					.anyRequest().authenticated()
--- src/main/java/se/systementor/supershoppen1/shop/model/Subscriber.java
+++ src/main/java/se/systementor/supershoppen1/shop/model/Subscriber.java
@@ -16,7 +16,7 @@ import java.util.List;
 @Table(name = "subscriber")
 public class Subscriber {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer Id;
     @Column
     private String email;
