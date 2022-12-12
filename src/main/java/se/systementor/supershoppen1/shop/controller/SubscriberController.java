16a42119937a49030f69d0211a4e044c3055b9eb
Oskar Herdenberg <90776459+Oggilit0@users.noreply.github.com>
2022-12-12 14:42:06 +0100

    Check if email exists (#23)
    * added emailcheck to saveEmail method
    
    * Re-designed saveEmail to check if email exists and check regex formatting
    
    * made fronteend compatible with saveEmail and added visual elements to display result

--- src/main/java/se/systementor/supershoppen1/shop/controller/SubscriberController.java
+++ src/main/java/se/systementor/supershoppen1/shop/controller/SubscriberController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import se.systementor.supershoppen1.shop.model.Newsletter;
 import org.springframework.web.servlet.ModelAndView;
 import se.systementor.supershoppen1.shop.model.Subscriber;
 import se.systementor.supershoppen1.shop.services.SubscriberService;
@@ -26,12 +27,25 @@ public class SubscriberController {
     }
 
 
-    @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public String saveEmail(@RequestParam(name = "addingsub") String email) {
-        Subscriber subscriber = new Subscriber();
-        subscriber.setEmail(email);
-        subscriberService.save(subscriber);
-        return "redirect:/";
+    @RequestMapping(value="/save", method = RequestMethod.POST)
+    public String saveEmail(@ModelAttribute("Newsletter")Subscriber subscriber, Model model,
+                            @RequestParam(name = "addingsub") String email){
+
+        String emailRegex = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+        boolean exists = checkIfEmailExists(email);
+        if(exists){
+            model.addAttribute("checkEmail", exists);
+        }else if(email.matches(emailRegex)){
+            subscriber.setEmail(email);
+            subscriberService.save(subscriber);
+        }else{
+            model.addAttribute("checkEmail", exists);
+        }
+        return "home";
+    }
+
+    public boolean checkIfEmailExists(String email){
+        return subscriberService.checkIfEmailExists(email);
     }
 
     @ResponseBody
