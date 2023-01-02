db7db9bbf3038b14a233bf07318078c93474c05f
Mathilda <frknilsson@hotmail.com>
2023-01-02 14:59:09 +0100

    added email service

--- src/main/java/se/systementor/supershoppen1/shop/services/EmailServiceImpl.java
+++ src/main/java/se/systementor/supershoppen1/shop/services/EmailServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 @Component
-public class EmailServiceImpl implements EmailService{
+public class EmailServiceImpl implements EmailService {
 
     @Autowired
     private JavaMailSender emailSender;
