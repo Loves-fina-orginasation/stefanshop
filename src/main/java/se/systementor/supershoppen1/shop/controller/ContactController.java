7642e28aa8e13434767e46e31a7baf6ca458801c
larashit <laraibrahim@studerande.plushogskolan.se>
2023-01-05 14:24:48 +0100

    Edit
    removed unused imports

--- src/main/java/se/systementor/supershoppen1/shop/controller/ContactController.java
+++ src/main/java/se/systementor/supershoppen1/shop/controller/ContactController.java
@@ -4,18 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-<<<<<<< Updated upstream
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 import se.systementor.supershoppen1.shop.model.Contact;
-=======
->>>>>>> Stashed changes
+
 import se.systementor.supershoppen1.shop.services.ContactService;
 
 @Controller
-//@RequestMapping("/contact")
+
 public class ContactController {
 
     private final ContactService contactService;
@@ -24,7 +18,7 @@ public class ContactController {
     public ContactController(ContactService contactService) {
         this.contactService = contactService;
     }
-<<<<<<< Updated upstream
+
     
     @GetMapping("/contact")
     public String form (Model model) {
@@ -32,14 +26,7 @@ public class ContactController {
         model.addAttribute("contact", contact);
         return "contact";
     }
+    
 
-=======
-
-    @GetMapping(path = "/admin/contact")
-    public String form(Model model) {
-        model.addAttribute("contact");
-        return "admin/contact";
-    }
->>>>>>> Stashed changes
 
 }
