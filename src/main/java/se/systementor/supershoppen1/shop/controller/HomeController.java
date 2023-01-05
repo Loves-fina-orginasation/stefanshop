f4b06640003863b3fb14ae515ba9fbf2bd193e6e
Oggilit0 <oskar.herdenberg@gmail.com>
2023-01-05 15:31:23 +0100

    Added backend methods to get single article from api

--- src/main/java/se/systementor/supershoppen1/shop/controller/HomeController.java
+++ src/main/java/se/systementor/supershoppen1/shop/controller/HomeController.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import se.systementor.supershoppen1.shop.model.Product;
 import se.systementor.supershoppen1.shop.services.ProductService;
@@ -34,6 +37,20 @@ public class HomeController {
         RestTemplate restTemplate = new RestTemplate();
         Object[] result = restTemplate.getForObject(url, Object[].class);
         model.addAttribute("krisinformation", Arrays.asList(result));
+    }
+
+    public Model getSingleKrisArticle(Model model, String id){
+        String url = "https://api.krisinformation.se/v3/features/"+id;
+        RestTemplate restTemplate = new RestTemplate();
+        Object result = restTemplate.getForObject(url, Object.class);
+        return model.addAttribute("krisArticle", result);
+    }
+
+    @GetMapping(path="/kris-information")
+    public String krisinfo(Model model,
+                           @RequestParam(value = "id") String articleId){
+        getSingleKrisArticle(model, articleId);
+        return "kris-information";
     }
 
     @GetMapping(path="/test2")
