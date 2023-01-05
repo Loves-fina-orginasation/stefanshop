6cd63af9853f900c03a07d5ed7168e9adc4c1200
Mathilda <frknilsson@hotmail.com>
2023-01-05 15:44:26 +0100

    added test class

---
+++ src/test/java/se/systementor/supershoppen1/shop/services/EmailServiceTest.java
@@ -0,0 +1,13 @@
+package se.systementor.supershoppen1.shop.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmailServiceTest {
+
+    @Test
+    void sendSimpleEmail() {
+        
+    }
+}
